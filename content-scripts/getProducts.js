2e5368a5a09c0b795765014a5ef8c78c8c5e5de9
mk210 <55575735+mk210@users.noreply.github.com>
2021-09-15 08:18:08 +0400

    added init code for fetching Product Data.

--- content-scripts/getProducts.js
+++ content-scripts/getProducts.js
@@ -1,12 +1,30 @@
-console.log('RUNNING GETPRODUCTS SCRIPT')
+console.log("RUNNING GETPRODUCTS SCRIPT");
 
 const getProducts = async () => {
-    //Returns array of product objects
+  //Returns array of product objects
+  console.log("inside function");
+  let headersList = {
+    Accept: "*/*",
+    "User-Agent": "www.vinted.co.uk",
+  };
 
-}
+  fetch(
+    "https://www.vinted.co.uk/api/v2/users/59783356/items?page=1&per_page=20&order=relevance¤cy=GBP",
+    {
+      method: "GET",
+      headers: headersList,
+    }
+  )
+    .then(function (response) {
+      return response.text();
+    })
+    .then(function (data) {
+      //   console.log(Object.keys(data.items).length);
+      console.log(data.items);
+    });
+};
 
-//Please catch any errors 
-getProducts()
-.then((data)=>{
-    console.log(data)
-})
+//Please catch any errors
+getProducts().then((data) => {
+  console.log(data);
+});
