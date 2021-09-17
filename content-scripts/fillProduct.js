2646e78e131d78f66ab236a755d4bfbee1ae08aa
mk210 <55575735+mk210@users.noreply.github.com>
2021-09-17 22:10:19 +0400

    added code to automatically add text to new items page and click SAVE draft.

--- content-scripts/fillProduct.js
+++ content-scripts/fillProduct.js
@@ -8,10 +8,16 @@ const product = {
 
 const fillProduct = async () => {
   //Fills form
+  document.getElementById("title").setAttribute("value", product.title);
+  document
+    .getElementById("description")
+    .setAttribute("value", product.description);
+  document.getElementById("description").innerHTML = product.description;
+  document
+    .getElementById("price")
+    .setAttribute("value", "$" + product.price + "." + 00);
 
-  document.getElementById("title").defaultValue = product.title;
-  document.getElementById("description").defaultValue = product.description;
-  document.getElementById("price").value = "£" + product.price + ".00";
+  document.getElementsByTagName("button")[0].click();
 };
 
 //Please catch any errors
