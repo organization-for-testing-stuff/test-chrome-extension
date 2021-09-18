a2ab9b96cae3d6e9133ec77f8ff24356b5739735
mk210 <55575735+mk210@users.noreply.github.com>
2021-09-18 14:07:44 +0400

    Added code to manipulate the form and save items

--- content-scripts/fillProduct.js
+++ content-scripts/fillProduct.js
@@ -6,18 +6,33 @@ const product = {
   price: "50",
 };
 
+function setNativeValue(element, value) {
+  let lastValue = element.value;
+  element.value = value;
+  let event = new Event("input", { target: element, bubbles: true });
+  // React 15
+  event.simulated = true;
+  // React 16
+  let tracker = element._valueTracker;
+  if (tracker) {
+    tracker.setValue(lastValue);
+  }
+  element.dispatchEvent(event);
+}
+
 const fillProduct = async () => {
   //Fills form
-  document.getElementById("title").setAttribute("value", product.title);
-  document
-    .getElementById("description")
-    .setAttribute("value", product.description);
-  document.getElementById("description").innerHTML = product.description;
-  document
-    .getElementById("price")
-    .setAttribute("value", "$" + product.price + "." + 00);
+  var title = document.getElementById("title");
+  setNativeValue(title, product.title);
 
-  document.getElementsByTagName("button")[0].click();
+  var description = document.getElementById("description");
+  setNativeValue(description, product.description);
+
+  var price = document.getElementById("price");
+  setNativeValue(price, product.price);
+
+  var SaveDraft = document.getElementsByClassName("Button_button__1HmfN");
+  SaveDraft.click();
 };
 
 //Please catch any errors
